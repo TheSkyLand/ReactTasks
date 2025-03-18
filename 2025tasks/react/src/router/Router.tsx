b5074aed4975356c9711374c0c34a444618429d1
George <77775699+TheSkyLand@users.noreply.github.com>
2025-03-19 00:11:12 +0300

    Add files via upload

---
+++ 2025tasks/react/src/router/Router.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
+import MainPage from '../pages/MainPage';
+import SecondPage from '../pages/SecondPage';
+import ErrorPage from '../pages/ErrorPage';
+import ThirdPage from '../pages/ThirdPage';
+
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path="/" index element={<MainPage />} />
+        <Route path="/second" index element={<SecondPage />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/third" element={<ThirdPage />} />
+    </Routes>
+  );
+};
+
+export default Router;
