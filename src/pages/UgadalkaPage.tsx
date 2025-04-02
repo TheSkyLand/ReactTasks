b105de8bc63332a2e5fe9cdcb75125a7fe63fb7c
TheSkyLand <Aradioactiveplay@gmail.com>
2025-04-03 00:04:12 +0300

    porabotal(skorozakonchu)

--- src/pages/UgadalkaPage.tsx
+++ src/pages/UgadalkaPage.tsx
@@ -1,42 +1,49 @@
 import { kMaxLength } from "buffer";
 import React from "react";
+import { arrayBuffer } from "stream/consumers";
 
 
 const UgadalkaPage = () => {
     const [inputNumber, setInputNumber] = React.useState(0)
+    const [arr, setArr] = React.useState([0])
 
-    let arr = []
-    for (let i = 0; i < 101; i++) {
-        arr[i] = i
+    for (let i = 0; i < 100; i++) {
+        arr[i] = i + 1
     }
 
+    function changeArr(event: any) {
+        setArr(event.target.value)
+    }
 
     function smaller() {
-        for(let i = 0; i < 50; i++)
-        {
-            arr[i] = i
-        }
-        console.log(arr.length)
+        Math.round(arr.length /= 2)
+       
+        console.log(arr)
     }
     function bigger() {
-        for(let i = 50; i < 101; i++)
-            {
-                arr.splice(i)
-            }
-        console.log(arr.length)
+        for(let i = 50; i < 100; i++){
+            arr[i] = i + 1
+
+        }
+
+        console.log(arr)
+    }
+    function endgame() {
+
     }
     return (<div>
-        <span> ваше число {Math.max.apply(null, arr)}?
+
+        <span onChange={(e) => changeArr(e)}> ваше число {Math.max.apply(null, arr)} ?
         </span>
-        <div>
-            <button >
+        <div style={{display:"flex", width:"100px",}}>
+            <button style={{border:"5px black solid" }} onClick={endgame}>
                 да
             </button>
-            <button onClick={bigger}>
-                нет оно больше
+            <button style={{border:"5px black solid" }} onClick={bigger} >
+                нет оно БОЛЬШЕ
             </button>
-            <button onClick={smaller}>
-                нет оно меньше
+            <button style={{border:"5px black solid"}} onClick={smaller} >
+                нет оно МЕНЬШЕ
             </button>
 
         </div>
