7a5c86b57f0239d3bdfdf5b27e8b1092f9d9d220
theskyland <aradioactiveplay@gmail.com>
2025-04-08 11:40:13 +0300

    porablota

--- src/pages/UgadalkaPage.tsx
+++ src/pages/UgadalkaPage.tsx
@@ -2,22 +2,23 @@ import React from "react";
 import { Button } from "@mui/material";
 
 
+function generateArray(arr: number[]) {
+    for (let i = 1; i < 101; i++) {
+        arr.push(i)
+    }
+    console.log(arr)
+}
 
 
 const UgadalkaPage = () => {
     const [answer, setAnswer] = React.useState([50])
     const [start, setStart] = React.useState([1])
     const [end, setEnd] = React.useState([100])
-    const [predict, setPredict] = React.useState("ваше число `${}`")
-    const [arr, setArr] = React.useState([])
-    function generateArray()
-    {
-        for (let i = 1; i > 101; i++) {
-            let arr = []
-            arr.push(i)
-            console.log(arr)
-        }
-    }
+    const [arr, setArr] = React.useState([0])
+    const [predict, setPredict] = React.useState(`${arr}`)
+
+
+
 
 
 
@@ -27,21 +28,23 @@ const UgadalkaPage = () => {
     function selectButton(key: number) {
         switch (key) {
             case 1:
+                generateArray(arr)
 
-                console.log("2")
+                console.log(arr)
                 break;
             case -1:
-                console.log("1")
+                console.log(arr)
                 break;
             default:
-                console.log("3")
+
+                console.log(arr)
                 break;
         }
     }
 
 
     return (
-        <div>
+        <div onChange={(e) => setPredict}>
             ваше число {predict}?
             <span> </span>
             <Button onClick={() => selectButton(0)}>Да, это моё число </Button>
