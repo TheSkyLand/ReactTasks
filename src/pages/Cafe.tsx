cdbfc2f005ad1e79b2728e7aac9d599175c50a83
theskyland <aradioactiveplay@gmail.com>
2025-04-17 13:10:16 +0300

    120-61

--- src/pages/Cafe.tsx
+++ src/pages/Cafe.tsx
@@ -1,39 +1,50 @@
+import { debug } from "console";
 import React from "react";
 
 const Cafe = () => {
     const [money, setMoney] = React.useState('')
     const [days, setDays] = React.useState([''])
     const [coupons, setCoupons] = React.useState([''])
-    let arr = []
+    let array = [15, 37, 60, 101, 12, 58, 102, 3, 100];
+    let count = 0;
+    let y: any = []
+    let p: any = []
+
+    /*
     function changeMoney(event: any) {
         setMoney(event.target.value)
     }
-
-
-
     function addEl() {
         days.splice(1, 0, money)
         console.log(days)
     }
+*/
+    function submit(arr: any) {
+        for (let i = 0; array.length > i; i++) {
+            if (Number(array[i]) >= 100) {
 
-    function submit() {
-        for (let i = 0; Array.length > i; i++) {
-            if (Number(days[i]) >= 101) {
-                
-                days.concat(coupons)
+
+                y.push(i)
+                count++
+
             }
         }
-        console.log(coupons)
+        let maxEl = arr[y[0] + 1] 
+        let maxIndx = y[0] + 1
+        while (array.length < 1) {
+            for (let j = y[0]; y[0]; j++) {
+                maxEl[j] = arr[j + 1] 
+
+
+            }
+        }
+
+        console.log(array)
+
     }
     return (
         <div>
-
-            <span>vvedite chislo</span>
-            <input value={money} onInput={(e) => changeMoney(e)}>
-
-            </input>
-            <button style={{ border: "2px black solid" }} onClick={addEl}>add</button>
-            <button style={{ border: "2px black solid" }} onClick={submit}>submit</button>
+            <button onClick={() => submit(array)}>test</button>
         </div>
     )
 }
