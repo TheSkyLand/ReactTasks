3d6a746a285f569a6d06f563be7205e0509b9ba6
George <77775699+TheSkyLand@users.noreply.github.com>
2025-05-13 15:05:30 +0300

    Update Cafe.tsx

--- src/pages/Cafe.tsx
+++ src/pages/Cafe.tsx
@@ -1,58 +1,86 @@
-import { debug } from "console";
+import { Box, Button, Input } from "@mui/material";
 import React from "react";
 
 const Cafe = () => {
     const [money, setMoney] = React.useState('')
     const [days, setDays] = React.useState([''])
     const [coupons, setCoupons] = React.useState([''])
-    const [array, setArray] = React.useState([''])
-    let count = 0;
-    let y: any = []
-    let p: any = []
+    //let array = [15, 37, 60, 101, 12, 58, 102, 3, 100];
+    let y: any = [];
+    let p: any = [];
+    let count = 0
 
-    function addEl(money: any) {
-        array.splice(1, 0, money)
-        console.log(array)
+    function changeMoney(event: any) {
+        setMoney(event.target.value)
     }
 
-    function submit(arr: any) {
-        for (let i = 0; array.length > i; i++) {
-            if (Number(array[i]) >= 100) {
 
 
-                y.push(i)
+    function addEl() {
+        days.push(money)
+        console.log(days)
+    }
+
+    function testTest(arr: any) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Number(arr[i]) >= 100) {
+                y.push(i);
                 count++
-
+                coupons.push(arr)
             }
         }
-        let maxEl = arr[y[0] + 1]
-        let maxIndx = y[0] + 1
         while (y.length > 1) {
             let maxEl = arr[y[0] + 1];
-            let maxIndx = y[0] + 1;
+            let maxInd = y[0] + 1;
             for (let j = y[0] + 1; j < y[1]; j++) {
                 if (maxEl < arr[j]) {
                     maxEl = arr[j];
-                    maxIndx = j;
+                    maxInd = j;
                 }
             }
-            p.push(maxIndx);
+            p.push(maxInd);
 
             y.splice(0, 1);
         }
-        console.log(array)
-        console.log(y)
         console.log(p)
+        console.log(y)
+        console.log(days)
+
     }
 
+
+    function submit(arr: any) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Number(arr[i]) >= 100) {
+                y.push(i);
+                count++
+                coupons.push(arr)
+            }
+        }
+        while (y.length > 1) {
+            let maxEl = arr[y[0] + 1];
+            let maxInd = y[0] + 1;
+            for (let j = y[0] + 1; j < y[1]; j++) {
+                if (maxEl < arr[j]) {
+                    maxEl = arr[j];
+                    maxInd = j;
+                }
+            }
+            p.push(maxInd);
+
+            y.splice(0, 1);
+        }
+    }
     return (
-        <div>
+        <Box>
 
-            <input type="number" style={{ border: "1px black solid" }} value={money} onChange={(e) => setMoney(e.target.value)} />
-            <button style={{ border: "1px black solid" }} onClick={() => addEl(array)}>добавить</button>
+            <span>vvedite chislo</span>
+            <Input value={money} onInput={(e) => changeMoney(e)} />
 
-            <button onClick={() => submit(array)}>test</button>
-        </div>
+            <Button style={{ border: "2px black solid" }} onClick={addEl}>add</Button>
+            //<Button style={{ border: "2px black solid" }} onClick={() => submit(array)}>submit</Button>
+            <Button onClick={() => testTest(days)}>test </Button>
+        </Box>
     )
 }
 
