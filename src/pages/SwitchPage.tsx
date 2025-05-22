988aeaaa2eb489c37a728b544a7a23440b28b015
theskyland <aradioactiveplay@gmail.com>
2025-05-22 15:03:01 +0300

    POMOGITE1!!!1!! V2

--- src/pages/SwitchPage.tsx
+++ src/pages/SwitchPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Input, TextField, Typography } from "@mui/material"
+import { Box, Button, Input, Modal, TextField, Typography } from "@mui/material"
 import React, { Component } from "react"
 import "./mainstyles/styles.css"
 import Numpad from "../components/Numpad"
@@ -7,8 +7,11 @@ import NumberField from "../components/NumberField"
 import { ProductsData } from "../helpers/ProductsData"
 import { clear } from "@testing-library/user-event/dist/clear"
 
-
 const SwitchPage = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
 
     let p = [];
 
@@ -22,23 +25,50 @@ const SwitchPage = () => {
         setInput(InputNum + value.toString());
     }
 
-    
-    function confirm() {
-        const ExitData = ProductsData.filter(code => backNum)
-        
-        return alert(ExitData)
+
+
+    function Data() {
+        ProductsData.find(function (item) {
+            handleOpen()
+
+            if (item.code.toString() === InputNum) {
+                return 
+            }
+        })
     }
-    
-   console.log(ProductsData)
+
 
     return (
         <div className="main">
             <h1 className="main-title">Автомат симулятор</h1>
             <div>
                 <Typography>окно с товарами</Typography>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <Box sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        border: '2px solid #000',
+                        boxShadow: 24,
+                        p: 4,
+                    }}
+                    >
+                        <Typography>
+                            {}
+                        </Typography>
+                    </Box>
+
+                </Modal>
+
 
                 <NumberField
-                inputStr={InputNum}
+                    inputStr={InputNum}
 
 
                 />
@@ -54,7 +84,8 @@ const SwitchPage = () => {
                         />
                     ))}
                     <Button
-                    onClick={() => confirm()}
+                        onClick={() => Data()}
+
 
                         sx={{
                             color: "green"
@@ -77,20 +108,20 @@ const SwitchPage = () => {
                     height: "1000px",
                     border: "black solid 1px"
                 }}>
-                    {ProductsData.map((item, key) => ( 
-                    <Products
-                        image={`${process.env.PUBLIC_URL + item.img}`}
-                        cost={item.cost}
-                        code={item.code}
+                    {ProductsData.map((item, key) => (
+                        <Products
+                            image={`${process.env.PUBLIC_URL + item.img}`}
+                            cost={item.cost}
+                            code={item.code}
                         />
-                    ))} 
+                    ))}
                 </Box>
 
             </div>
             <div className="main-numpad">
 
             </div>
-        </div>
+        </div >
 
 
     )
