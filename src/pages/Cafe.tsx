7b33b1589c94d75e7c71142d74d9f9ab6a1596d8
theskyland <aradioactiveplay@gmail.com>
2025-06-10 14:25:52 +0300

    sal;gkas;k

--- src/pages/Cafe.tsx
+++ src/pages/Cafe.tsx
@@ -3,18 +3,17 @@ import React from "react";
 import { Simulate } from "react-dom/test-utils";
 
 const Cafe = () => {
-    const [money, setMoney] = React.useState('')
-    const [days, setDays] = React.useState([''])
-    const [coupons, setCoupons] = React.useState([''])
+    const [money, setMoney] = React.useState(0)
+    const [days, setDays]: any = React.useState([])
+    const [coupons, setCoupons] = React.useState([])
     let array = [15, 37, 60, 101, 12, 58, 102, 3, 100];
     let y: any = []
     let p: any = []
     let count = 0
     let summ = 0
 
-
     function changeMoney(event: any) {
-        setMoney(event.target.value.toString())
+        setMoney(Number(event.target.value))
     }
 
     function changeDays(event: any) {
@@ -22,12 +21,13 @@ const Cafe = () => {
     }
 
 
-    function addEl(event: any) {
+    function addEl(days: any, money: any) {
         days.push(money)
         console.log(days)
+        return days
     }
 
-    function testTest(arr: any) {
+    function testTest(arr: any, coupons: any) {
 
         for (let i = 0; i < arr.length; i++) {
             if (Number(arr[i]) >= 100) {
@@ -35,31 +35,37 @@ const Cafe = () => {
                 count++
                 coupons.push(arr)
             }
+
         }
         while (y.length > 1) {
             let maxEl = arr[y[0] + 1];
             let maxInd = y[0] + 1;
+
             for (let j = y[0] + 1; j < y[1]; j++) {
                 if (maxEl < arr[j]) {
                     maxEl = arr[j];
                     maxInd = j;
                 }
+
+
             }
             p.push(maxInd);
             y.splice(0, 1);
 
-            for (let i = p; i < arr.length; i++) {
-                debugger
-                summ += arr[i]
-            }
         }
+        for (let i = 0; i < arr.length; i++) {
+            summ += arr[i]
+        }
+
         console.log(`дни: ${arr}`)
         console.log(`Дни в которые использованы купоны: ${p}`)
         console.log(`дни когда купон получен: ${y}`)
         console.log(`Количество купонов: ${count}`)
         console.log(`Сумма ${summ}`)
 
+        return coupons
     }
+
     return (
         <Box>
 
@@ -71,8 +77,8 @@ const Cafe = () => {
             </ListItem>
 
 
-            <Button style={{ border: "2px black solid" }} onClick={(e) => addEl(e)}>add</Button>
-            <Button onClick={() => testTest(days)}>test </Button>
+            <Button style={{ border: "2px black solid" }} onClick={(e) => addEl(days, money)}>add</Button>
+            <Button onClick={() => testTest(days, coupons)}>test </Button>
         </Box>
     )
 }
