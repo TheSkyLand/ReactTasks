ef7ac555cb62a2408fddb129e3a52a786592742a
George <77775699+TheSkyLand@users.noreply.github.com>
2025-12-02 11:33:06 +0300

    Update CafeNew.tsx

--- src/pages/CafeNew.tsx
+++ src/pages/CafeNew.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 const CafeNew = () => {
-    let arr = [5, 35, 40, 101, 59, 63]
+    const [arr, setArr] = React.useState([5, 35, 40, 101, 59, 63])
 
-    let p: any = []; //массив в котором находятся купоны больше 101
+    let p: any = [] //массив в котором находятся купоны больше 101
+
+    let b: any = []
 
     let sum = 0;
 
     let count = 0; //количество купонов
 
     let max = arr[0]; //создаём переменную для хранения максимального числа
+    let min = arr[0]
 
     const test = () => {
         debugger
@@ -17,30 +20,34 @@ const CafeNew = () => {
             if (arr[i] >= 100) {
                 count++
             }
-            if (max < arr[i] || max != 100) {
+            if (max < arr[i] || max !== 100) {
                 max = arr[i]
+                if (min > arr[i]) {
+                    min = arr[i]
+                }
             }
         }
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i] != max) {
+            if (arr[i] !== max && count > 0 && arr[i] !== min) {
                 p.push(arr[i])
             }
         }
         for (let i = 0; i < arr.length; i++) {
-            sum += arr[i];
+            if (arr[i] === p[i - 1]) {
+                sum += arr[i]
+            }
         }
 
-        console.log("дни с использованием купона:", p)
+        console.log("дни с использованием купона: ", p)
 
-        console.log("день в который был получен купон:", max)
+        console.log("день в который был использован купон: ", max)
 
-        console.log("дни с купоном:", arr)
+        console.log("количество купонов: ", count)
 
-        console.log("сумма", sum)
+        console.log("неделя: ", arr)
+
+        console.log("сумма ", sum)
     }
-
-
-
 
     return (
         <div>
